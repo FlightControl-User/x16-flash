814b95cb6d698cebef58d5c14ef89f6d870a7870
Sven Van de Velde <sven.van.de.velde@telenet.be>
2023-10-17 13:00:54 +0200

    comments and credits

--- src/cx16-defines.h
+++ src/cx16-defines.h
@@ -1,9 +1,9 @@
 /**
  * @file cx16-defines.h
  * 
- * @author Wavicle from CX16 community (https://gist.github.com/jburks) -- Main ROM update logic & overall support and test assistance.
- * @author MooingLemur from CX16 community (https://github.com/mooinglemur) -- Main SPI and VERA update logic, VERA firmware.
- * @author Stefan Jakobsson from CX16 community (https://github.com/stefan-b-jakobsson) -- Main SMC update logic, SMC firmware and bootloader.
+ * @author Wavicle from CX16 community (https://gist.github.com/jburks) -- Advice and outline of the ROM update logic & overall support and test assistance of this program.
+ * @author MooingLemur from CX16 community (https://github.com/mooinglemur) -- Advice and outline of the main SPI and W25Q16 update logic, and supply of new VERA firmware.
+ * @author Stefan Jakobsson from CX16 community (https://github.com/stefan-b-jakobsson) -- Advice and outline of the SMC update logic, SMC firmware and bootloader.
  * @author Sven Van de Velde from CX16 community (https://github.com/FlightControl-User) -- Creation of this program, under the strong expertise by the people above.
  * 
  * @brief COMMANDER X16 GLOBAL DEFINES AND CONDITIONAL COMPILE
